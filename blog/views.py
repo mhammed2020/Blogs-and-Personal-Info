97fa430391a08d6cff4d60319c3ca98120f2de47
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-29 23:58:14 +0100

    Create users app for registration feature

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-# posts = [
-#     {
-#         'author': 'jeddou ',
-#         'title': 'Blog Post 1',
-#         'content': 'First post content',
-#         'date_posted': 'August 27, 2020'
-#     },
-#     {
-#         'author': 'mhammed Doe',
-#         'title': 'Blog Post 2',
-#         'content': 'Second post content',
-#         'date_posted': 'August 28, 2020'
-#     }
-# ]
+
 
 
 def home(request):

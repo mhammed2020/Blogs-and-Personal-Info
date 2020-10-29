09a56f22cc9444201f4775b46106f3d01befb60d
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-29 23:33:11 +0100

    Create Post model and makemigrations

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'jeddou ',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2020'
-    },
-    {
-        'author': 'mhammed Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2020'
-    }
-]
+from .models import Post
+# posts = [
+#     {
+#         'author': 'jeddou ',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2020'
+#     },
+#     {
+#         'author': 'mhammed Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28, 2020'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 

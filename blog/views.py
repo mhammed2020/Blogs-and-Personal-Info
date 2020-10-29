b5dae953e4b54efdeb36aba49f83e4a609ca3376
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-29 20:35:12 +0100

    Analyse and build  MVT pattern

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'jeddou ',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2020'
+    },
+    {
+        'author': 'mhammed Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2020'
+    }
+]
+
 
 def home(request):
-    
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
-    return render(request,'blog/home.html',{})
 
 def about(request):
-    
-
-    return render(request,'blog/about.html',{})
+    return render(request, 'blog/about.html', {'title': 'About'})
